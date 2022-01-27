009de1e296dd182d79ca466daa979032b63499c4
rhit-jiangq1 <73331485+rhit-jiangq1@users.noreply.github.com>
2022-01-27 16:14:13 -0500

    Update CreateListener.java

--- src/Listener/CreateListener.java
+++ src/Listener/CreateListener.java
@@ -4,6 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CreateListener implements ActionListener {
+	
+	TrainerID
+	Name
+	Gender
+	AbilityID
+	Specie
+	Level
+	
+	
+	
     @Override
     public void actionPerformed(ActionEvent e) {
 
